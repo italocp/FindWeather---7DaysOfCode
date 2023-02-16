dbbcedac502ce7dbbf4b99cafb5e8cdbe84446b4
Italocp <italopereiracp@gmail.com>
2023-02-16 11:23:59 -0300

    add fonts

--- App.tsx
+++ App.tsx
@@ -1,6 +1,22 @@
 import { StatusBar, Text, View } from "react-native";
+import {
+  useFonts,
+  Overpass_400Regular,
+  Overpass_600SemiBold,
+  Overpass_700Bold,
+} from "@expo-google-fonts/overpass";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Overpass_400Regular,
+    Overpass_600SemiBold,
+    Overpass_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <>
       <StatusBar barStyle="default" />
@@ -14,6 +30,8 @@ export default function App() {
       >
         <Text
           style={{
+            fontFamily: "Overpass_400Regular",
+            fontSize: 30,
             color: "#fff",
           }}
         >
